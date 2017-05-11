0822ceb0f3eb0ccd03241e52f259fe387c25145b
MasahiroSaito <MasahiroSaito@users.noreply.github.com>
2017-05-11 11:51:36 +0900

    Update prob1/main

--- prob1/main.c
+++ prob1/main.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c;
-    int max;
+    int a, b, c, d;
+    int min;
+
+    puts("bXXXXXXX hogehoge piyo");
 
     printf("Value of a:"); scanf("%d", &a);
     printf("Value of b:"); scanf("%d", &b);
     printf("Value of c:"); scanf("%d", &c);
+    printf("Value of d:"); scanf("%d", &d);
 
-    max = a;
+    min = a;
 
-    if (b > max) max = b;
-    if (c > max) max = c;
+    if (b < min) min = b;
+    if (c < min) min = c;
+    if (d < min) min = d;
 
-    printf("Maximum : %d\n", max);
+    printf("Minimum : %d\n", min);
 
     return 0;
 }
